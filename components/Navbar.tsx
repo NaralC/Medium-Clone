32deffaad8c432efeaeb2c6df09ca51d7f269993
Naral <77269201+NaralC@users.noreply.github.com>
2023-04-08 02:22:02 +0100

    better navbar haha 💀

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,16 @@
 import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/router";
-import { Button } from "../components/Button"
+import { Button } from "../components/Button";
 import Link from "next/link";
+import { useState } from "react";
+import { Disclosure } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
+const navigation = [
+  { name: "Home", href: "/", current: true },
+  { name: "Main Feed", href: "/mainFeed", current: true },
+  { name: "Create Article", href: "/createArticle", current: false },
+];
 
 export default function Navbar() {
   const supabaseClient = useSupabaseClient();
@@ -15,34 +24,74 @@ export default function Navbar() {
 
   return (
     <>
-      <header className="z-50 flex flex-wrap w-full py-4 text-sm sm:justify-start sm:flex-nowrap dark:bg-gray-800">
-        <nav
-          className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between"
-          aria-label="Global"
-        >
-          <Link href="/">💀 Branding 💀</Link>
-          <div className="flex flex-row items-center gap-5 mt-5 sm:justify-end sm:mt-0 sm:pl-5">
-            <Link href="/mainFeed">Main Feed</Link>
-            <Link href="/createArticle">Create Article</Link>
-            {!user ? (
-              <>
-                <Link href="/login">
-                  <Button color="green">Login</Button>
-                </Link>
-              </>
-            ) : (
-              <>
-                <div>Hey, {user?.email}</div>
-                <div>
-                  <button onClick={() => signOutUser()}>
-                    Logout
-                  </button>
+      <Disclosure as="nav" className="bg-gray-800 min-w-[250px]">
+        {({ open }) => (
+          <>
+            <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
+              <div className="relative flex items-center justify-between h-16">
+                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                  {/* Mobile menu button*/}
+                  <Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+                      <XMarkIcon className="block w-6 h-6" aria-hidden="true" />
+                    ) : (
+                      <Bars3Icon className="block w-6 h-6" aria-hidden="true" />
+                    )}
+                  </Disclosure.Button>
                 </div>
-              </>
-            )}
-          </div>
-        </nav>
-      </header>
+                <div className="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start">
+                  <div className="flex items-center flex-shrink-0">
+                    <div className="block w-auto h-8 lg:hidden">🤓🤓🤓</div>
+                    <div className="hidden w-auto h-8 lg:block">🤓🤓🤓</div>
+                  </div>
+                  <div className="hidden sm:ml-6 sm:block">
+                    <div className="flex space-x-4">
+                      {navigation.map((item) => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
+                          aria-current={item.current ? "page" : undefined}
+                        >
+                          {item.name}
+                        </a>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+                <div className="absolute inset-y-0 right-0 flex items-center ">
+                  {!user ? (
+                    <Link href="/login">
+                      <Button>Login</Button>
+                    </Link>
+                  ) : (
+                    <Link href="/login">
+                      <Button onClick={signOutUser}>Logout</Button>
+                    </Link>
+                  )}
+                </div>
+              </div>
+            </div>
+
+            <Disclosure.Panel className="sm:hidden">
+              <div className="px-2 pt-2 pb-3 space-y-1">
+                {navigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
+                    aria-current={item.current ? "page" : undefined}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                ))}
+              </div>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
     </>
   );
 }
